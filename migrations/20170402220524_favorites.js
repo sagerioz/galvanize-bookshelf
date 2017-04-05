b3ff08918ce68351fee3b23b3fbd24d3d7c2fa55
sagerioz <ericaepperson27@yahoo.com>
2017-04-05 14:04:53 -0600

    changed migrations

--- migrations/20170402220524_favorites.js
+++ migrations/20170402220524_favorites.js
@@ -2,12 +2,12 @@
 exports.up = function(knex) {
   return knex.schema.createTable('favorites', (table) => {
     table.increments();
-    table.integer('book_id').notNullable().references('id').inTable('books').onDelete("CASCADE");
-    table.integer('user_id').notNullable().references('id').inTable('users').onDelete("CASCADE");
+    table.integer('book_id').notNullable().references('books.id').onDelete("CASCADE");
+    table.integer('user_id').notNullable().references('users.id').onDelete("CASCADE");
     table.timestamps(true, true);
   })
 };
 
 exports.down = function(knex) {
-
+  return knex.schema.dropTable('favorites');
 };
