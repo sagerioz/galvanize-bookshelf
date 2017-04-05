2c041373b35e14e473dd168d3cc9fb16948c3550
sagerioz <ericaepperson27@yahoo.com>
2017-04-05 14:10:24 -0600

    things being changed in users migrations

--- migrations/20170329211232_users.js
+++ migrations/20170329211232_users.js
@@ -1,15 +1,15 @@
-exports.up = function(knex) {
-  // create table
-  return knex.schema.createTable('users', (table) => {
+exports.up = (knex) => {
+  return knex.schema.createTable("users", (table) => {
+    // CREATING SERIAL PRIMARY KEY
     table.increments();
-    table.string('first_name').notNullable().defaultTo('');
-    table.string('last_name').notNullable().defaultTo('');
-    table.string('email').notNullable().unique();
-    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.string("first_name", 255).notNullable().defaultTo("");
+    table.string("last_name", 255).notNullable().defaultTo("");
+    table.string("email", 255).notNullable().unique();
+    table.specificType("hashed_password", "character(60)").notNullable();
     table.timestamps(true, true);
-  })
+  });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('users');
+exports.down = (knex) => {
+  return knex.schema.dropTable("users");
 };
